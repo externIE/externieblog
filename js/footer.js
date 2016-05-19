e1b50689f03ec7f751b00b5a0a0a18ee56845279
externIE <externIE@outlook.com>
2016-05-19 21:39:00 +0800

    newspost

--- js/footer.js
+++ js/footer.js
@@ -163,6 +163,7 @@ function collectLecture(cardEl){
 	var date = cardEl.getElementsByClassName("date")[0].innerHTML;
 	var time = cardEl.getElementsByClassName("time")[0].innerHTML;
 	var address = cardEl.getElementsByClassName("address")[0].innerHTML;
+	var url = cardEl.getElementsByClassName("modal-trigger")[0].href;
 	address = address.split("&nbsp;")[0];
 	//这里调用Android函数给Android返回卡片的信息
 	if (window.android) {
